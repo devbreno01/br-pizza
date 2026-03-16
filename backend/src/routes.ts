9fe866c5a0049622c67c95ea112ee8cf04368c14
Breno Guimarães <devbreno4@gmail.com>
2026-03-16 18:50:17 -0300

    chore: creating route file

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,7 +3,11 @@ import { Router, Request, Response } from "express";
 const router = Router(); 
 
 router.get("/user",(req: Request,res: Response)=>{
-    res.json({message: "vai puta"}); 
+    res.json({message: "teste"}); 
+})
+
+router.post("/user",(req: Request,res: Response)=>{
+    res.json({message: "teste"}); 
 })
 
 export { router }; 
