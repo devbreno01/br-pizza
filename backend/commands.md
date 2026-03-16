054864ef39efc6501636e3f1404cf1ddab37bd0e
Breno Guimarães <devbreno4@gmail.com>
2026-03-16 18:05:55 -0300

    chore: add backend files

---
+++ backend/commands.md
@@ -0,0 +1,5 @@
+npm install --save-dev typescript @types/node 
+npm install express
+npm install tsx 
+npm install --save-dev @types/express 
+npx tsc --init 
---
+++ backend/src/server.ts
@@ -0,0 +1,11 @@
+import express, {NextFunction, Request, Response} from "express"; 
+
+const app = express(); 
+
+app.use(express.json()); 
+
+const PORT = 3000; 
+
+app.listen(PORT, ()=>{
+    console.log("Servidor Rodando...");
+}); 
