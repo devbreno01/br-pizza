d62c4f5e9f215a201502227f7dbee543e1e7fab4
Breno Guimarães <devbreno4@gmail.com>
2026-04-07 21:54:44 -0300

    refactor: improve create category service

--- backend/src/services/category/CreateCategoryService.ts
+++ backend/src/services/category/CreateCategoryService.ts
@@ -15,16 +15,24 @@ class CreateCategoryService{
         if(!findCategory)
             throw new Error('Categoria já cadastrada'); 
 
-        const category = prismaClient.category.create({
-            data:{
-                name: name 
-            }, 
+        try{
+            const category = prismaClient.category.create({
+                data:{
+                    name: name 
+                }, 
 
-            select:{
-                name: true, 
-                createdAt: true
-            }
-        });
+                select:{
+                    name: true, 
+                    createdAt: true
+                }
+            });
+
+            return category; 
+        }catch(error){
+            throw new Error("Erro ao cadastrar categoria");
+        }        
 
     }
 }
+
+export {CreateCategoryService}
