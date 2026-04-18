48aba66a03eb40e51f0064e0cc31f2e9f64de26a
Breno Guimarães <devbreno4@gmail.com>
2026-04-18 10:04:15 -0300

    feat: list products

---
+++ backend/src/services/products/ListProductService.ts
@@ -0,0 +1,28 @@
+import prismaClient from "../../prisma"
+
+interface listCategoryProps{
+    disabled: boolean
+}
+
+class ListProductService {
+    
+
+    async execute({disabled}: listCategoryProps) {
+        const products = prismaClient.product.findMany({
+            
+            where:{
+                disabled: disabled
+            },
+
+            select:{
+                name: true, 
+                price: true, 
+                description: true, 
+                banner: true,
+                category_id: true
+            }
+        });
+        
+        return products; 
+    }
+}
