4d9f77805ca7961620f3eb3ef93b322e9c70dd7f
Breno Guimarães <devbreno4@gmail.com>
2026-05-04 19:35:39 -0300

    feat: register component

--- frontend/src/components/forms/register-form.tsx
+++ frontend/src/components/forms/register-form.tsx
@@ -1,9 +1,49 @@
+import {Card , 
+        CardHeader, 
+        CardDescription, 
+        CardContent, 
+        CardFooter , 
+        CardTitle} from "@/components/ui/card"; 
 
+import { Label } from "@/components/ui/label"; 
+import { Input } from "@/components/ui/input"; 
 
+ 
 export function RegisterForm(){
     return (
-        <div>
-            <h1 className="text-white">TESTEEE</h1>
-        </div>
+        <Card>
+            <CardHeader>
+                <CardTitle>Br<span className="text-brand-primary">Pizza</span></CardTitle>
+            </CardHeader>
+
+            <CardContent>
+                <form action="">
+                    <div>
+                        <Label htmlFor="name">Nome</Label>
+                        <Input type="text"
+                        id="name" 
+                        placeholder="Digite seu nome"
+                        required
+                        className="text-white bg-app-card border border-app-border">
+                        </Input>
+                    </div>
+                    <div>
+                        <Label htmlFor="email">E-mail</Label>
+                        <Input type="email"
+                        id="email" 
+                        placeholder="Digite seu e-mail"
+                        required></Input>
+                    </div>
+                    <div>
+                        <Label htmlFor="password">Password</Label>
+                        <Input type="password"
+                        id="password" 
+                        placeholder="Digite sua senha"
+                        required></Input>
+                    </div>
+                   
+                </form>
+            </CardContent>
+        </Card>
     );
 }
