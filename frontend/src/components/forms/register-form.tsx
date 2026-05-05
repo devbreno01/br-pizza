ce2aa1cdd41678237c34cf416aa254b5ed730ebb
Breno Guimarães <devbreno4@gmail.com>
2026-05-05 18:55:39 -0300

    feat: layout tela de registro

--- frontend/src/components/forms/register-form.tsx
+++ frontend/src/components/forms/register-form.tsx
@@ -7,19 +7,19 @@ import {Card ,
 
 import { Label } from "@/components/ui/label"; 
 import { Input } from "@/components/ui/input"; 
-
+import {Button} from "@/components/ui/button";
  
 export function RegisterForm(){
     return (
-        <Card>
+        <Card className="bg-app-card border-app-border w-full max-w-md mx-auto">
             <CardHeader>
-                <CardTitle>Br<span className="text-brand-primary">Pizza</span></CardTitle>
+                <CardTitle className="text-white text-center text-3xl sm:text-4xl font-bold">Br<span className="text-brand-primary">Pizza</span></CardTitle>
             </CardHeader>
 
             <CardContent>
-                <form action="">
-                    <div>
-                        <Label htmlFor="name">Nome</Label>
+                <form action="" className="space-y-4">
+                    <div className="space-y-2">
+                        <Label htmlFor="name" className="text-white">Nome</Label>
                         <Input type="text"
                         id="name" 
                         placeholder="Digite seu nome"
@@ -27,21 +27,26 @@ export function RegisterForm(){
                         className="text-white bg-app-card border border-app-border">
                         </Input>
                     </div>
-                    <div>
-                        <Label htmlFor="email">E-mail</Label>
+                    <div className="space-y-2 ">
+                        <Label htmlFor="email" className="text-white">E-mail</Label>
                         <Input type="email"
                         id="email" 
                         placeholder="Digite seu e-mail"
-                        required></Input>
+                        required
+                        className="text-white bg-app-card border border-app-border">
+                        
+                        </Input>
                     </div>
-                    <div>
-                        <Label htmlFor="password">Password</Label>
+                    <div className="space-y-2 ">
+                        <Label htmlFor="password" className="text-white">Password</Label>
                         <Input type="password"
                         id="password" 
                         placeholder="Digite sua senha"
-                        required></Input>
+                        required
+                        className="text-white bg-app-card border border-app-border"></Input>
                     </div>
-                   
+                   <Button type="submit" 
+                   className="w-full bg-brand-primary text-white hover:bg-brand-primary">Criar Conta</Button>
                 </form>
             </CardContent>
         </Card>
