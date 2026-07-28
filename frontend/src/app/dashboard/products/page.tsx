fdaf0a299575c3dbd4115b183bcb307601d7de18
Breno Guimarães <devbreno4@gmail.com>
2026-07-28 20:09:28 -0300

    wip

--- frontend/src/app/dashboard/products/page.tsx
+++ frontend/src/app/dashboard/products/page.tsx
@@ -1,17 +1,25 @@
 
 import ProductForm from "@/components/dashboard/product-form";
-
+import ProductList from "@/components/dashboard/product-list";
 export default function Products(){
 
     return (
-       <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-4">
-            <div>
-                <h1 className="text-2xl sm:text-3xl font-bold text-white">Produtos</h1>
-                <p className="text-sm sm:text-base mt-1">Gerencie o cardápio da pizzaria</p>
-            </div>
+        <div className="space-y-4 sm:space-y-6">
+            <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-4">
+                <div>
+                    <h1 className="text-2xl sm:text-3xl font-bold text-white">Produtos</h1>
+                    <p className="text-sm sm:text-base mt-1">Gerencie o cardápio da pizzaria</p>
+                </div>
      
-            <ProductForm/>
+                <ProductForm/>
+            
+            </div>
+
+            <div>
+                <ProductList/>
+            </div>
         </div>
+        
         
     ); 
 }
